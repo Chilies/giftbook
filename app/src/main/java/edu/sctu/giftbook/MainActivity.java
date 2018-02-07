eb79c77428a21dfd7a135469b58ebb35eeaf753d
zhengsenwen <673584794@qq.com>
2018-02-07 20:23:48 +0800

    20180207 first

--- app/src/main/java/edu/sctu/giftbook/MainActivity.java
+++ app/src/main/java/edu/sctu/giftbook/MainActivity.java
@@ -3,18 +3,18 @@ package edu.sctu.giftbook;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
